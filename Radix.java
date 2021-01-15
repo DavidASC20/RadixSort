912a315974918bad48a8b9e17a304e071f7d6ec5
David Chong <djchong24@gmail.com>
2021-01-15 00:00:07 -0500

    finished simple

--- Radix.java
+++ Radix.java
@@ -20,9 +20,31 @@ public class Radix {
         }
     }
 
-    public static void radixSortSimple(SortableLinkedList data){
-
-    }
+    
+        public static void radixSortSimple(SortableLinkedList data){
+            int nums=0;
+            for (int i=0;i< data.size();i++){
+              if (nums<data.get(i)){
+                nums=data.get(i);
+        
+              }
+            }
+            int i =0;
+            SortableLinkedList buckets[]=new SortableLinkedList[10];
+            for (int b=0;b<buckets.length;b++){
+              buckets[b]=new SortableLinkedList();
+            }
+            while (i<length(nums)){
+               while (data.size()>0){
+                 int x=data.remove(0);
+                int index= nth(x,i);
+                buckets[index].add(x);
+               }
+               merge(data,buckets);
+                  i++;
+            }
+          }
+    
 
     public static void radixSort(SortableLinkedList data){
         
